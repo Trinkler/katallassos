96f01c156703018044162467b83bed0e7ef26c71
brunoffranca88 <bruno@trinkler.software>
2019-05-06 16:45:04 +0200

    1) Added oracle module 2) Removed template module 3) Integrated oracle module into lib.rs (runtime)

--- katal/runtime/src/lib.rs
+++ katal/runtime/src/lib.rs
@@ -54,8 +54,8 @@ pub type BlockNumber = u64;
 /// Index of an account's extrinsic in the chain.
 pub type Nonce = u64;
 
-/// Used for the module template in `./template.rs`
-mod template;
+/// Used for the module oracle in './oracle.rs'
+mod oracle;
 
 /// Opaque types. These are used by the CLI to instantiate machinery that don't need to know
 /// the specifics of the runtime. They can then be made to be agnostic over specific formats
@@ -186,8 +186,8 @@ impl sudo::Trait for Runtime {
 	type Proposal = Call;
 }
 
-/// Used for the module template in `./template.rs`
-impl template::Trait for Runtime {
+/// Used for the module oracle in './oracle.rs'
+impl oracle::Trait for Runtime {
 	type Event = Event;
 }
 
@@ -204,8 +204,8 @@ construct_runtime!(
 		Indices: indices,
 		Balances: balances,
 		Sudo: sudo,
-		// Used for the module template in `./template.rs`
-		TemplateModule: template::{Module, Call, Storage, Event<T>},
+		//Used for the module oracle in './oracle.rs'
+		Oracle: oracle::{Module, Call, Storage, Event<T>},
 	}
 );
 
