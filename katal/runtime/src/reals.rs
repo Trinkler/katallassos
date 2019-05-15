122a6d60bb23723de344c563337a38f4d6b569c7
retotrinkler <reto@trinkler.software>
2019-05-15 13:50:46 +0200

    Reodering of functions in reals.rs

--- katal/runtime/src/reals.rs
+++ katal/runtime/src/reals.rs
@@ -27,18 +27,6 @@ impl Real {
     }
 }
 
-impl Neg for Real {
-    type Output = Real;
-
-    fn neg(self) -> Real {
-        if self.0.is_some() {
-            Real(self.0.unwrap().checked_neg())
-        } else {
-            Real(None)
-        }
-    }
-}
-
 impl Add for Real {
     type Output = Real;
 
@@ -51,12 +39,40 @@ impl Add for Real {
     }
 }
 
-impl Sub for Real {
+impl Div for Real {
     type Output = Real;
 
-    fn sub(self, rhs: Real) -> Real {
+    fn div(self, rhs: Real) -> Real {
         if self.0.is_some() && rhs.0.is_some() {
-            Real(self.0.unwrap().checked_sub(rhs.0.unwrap()))
+            // Casting onto larger type
+            let a: i128 = self.0.unwrap() as i128;
+            let b: i128 = rhs.0.unwrap() as i128;
+
+            // Checking for division by zero
+            if b == 0 {
+                return Real(None);
+            }
+
+            // Multiplying the dividend by the scale factor
+            let mut c = a * SF;
+
+            // Dividing by the divisor and calculating (double of) the remainder
+            let r = (c % b) * 2;
+            c /= b;
+
+            // Rounding away from zero
+            if r >= SF {
+                c += 1;
+            } else if r <= -SF {
+                c -= 1;
+            }
+
+            // Verifying if it over/underflows and then returning the appropriate answer
+            if c < MIN || c > MAX {
+                Real(None)
+            } else {
+                Real(Some(c as i64))
+            }
         } else {
             Real(None)
         }
@@ -98,40 +114,24 @@ impl Mul for Real {
     }
 }
 
-impl Div for Real {
+impl Neg for Real {
     type Output = Real;
 
-    fn div(self, rhs: Real) -> Real {
+    fn neg(self) -> Real {
+        if self.0.is_some() {
+            Real(self.0.unwrap().checked_neg())
+        } else {
+            Real(None)
+        }
+    }
+}
+
+impl Sub for Real {
+    type Output = Real;
+
+    fn sub(self, rhs: Real) -> Real {
         if self.0.is_some() && rhs.0.is_some() {
-            // Casting onto larger type
-            let a: i128 = self.0.unwrap() as i128;
-            let b: i128 = rhs.0.unwrap() as i128;
-
-            // Checking for division by zero
-            if b == 0 {
-                return Real(None);
-            }
-
-            // Multiplying the dividend by the scale factor
-            let mut c = a * SF;
-
-            // Dividing by the divisor and calculating (double of) the remainder
-            let r = (c % b) * 2;
-            c /= b;
-
-            // Rounding away from zero
-            if r >= SF {
-                c += 1;
-            } else if r <= -SF {
-                c -= 1;
-            }
-
-            // Verifying if it over/underflows and then returning the appropriate answer
-            if c < MIN || c > MAX {
-                Real(None)
-            } else {
-                Real(Some(c as i64))
-            }
+            Real(self.0.unwrap().checked_sub(rhs.0.unwrap()))
         } else {
             Real(None)
         }
