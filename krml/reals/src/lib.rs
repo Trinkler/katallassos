4d4f254da659a9fe3f1e456b9af50406a96f5758
brunoffranca88 <bruno@trinkler.software>
2019-05-23 21:06:13 +0100

    1- Updated README file 2- Put the reals library in its own crate and into the new KRML folder. 3- Prepared comments in the reals library to display in cargo docs. 4- Deleted from(u32) function in the reals library, it was redundant. 5- Reorganized the toml files in the node-template folder.

--- krml/reals/src/lib.rs
+++ krml/reals/src/lib.rs
@@ -1,22 +1,22 @@
-// The Reals library implements a new data type for safe fixed-point arithmetic. It works by creating
-// a struct containing only an option of an i64 and then doing operator overloading for the most
-// common arithmetic operations (addition, subtraction, multiplication, division). It also implements
-// some convenience functions like negation, absolute value and creating from an i64. It also allows
-// comparisons by deriving the Eq and Ord traits.
-//
-// Fixed point arithmetic works similarly to normal integer arithmetic but every number is scaled
-// by the same number, which we call the scale factor. This library allows to change the scale factor
-// by simply changing the constant SF. By default it is set to 1 billion, which gives reals with 9
-// decimal points.
-// Almost all operations works equally to integer arithmetic, except for multiplication and division.
-// In multiplication and division the result of the operation needs to be rescaled and rounded. The
-// range allowed by a real (for the default SF) is [-9223372036.854775808, 9223372036.854775807],
-// which is simply the range of an i64 but rescaled.
-//
-// This library also implements safe math. All reals are an option of an i64, so a real can have the
-// value 'None'. And all operations check for over/underflow and will return a 'None' as a result when
-// that happens. A quirk is that, when comparing two reals, 'None' is considered smaller than any
-// number.
+//! The Reals library implements a new data type for safe fixed-point arithmetic. It works by creating
+//! a struct containing only an option of an i64 and then doing operator overloading for the most
+//! common arithmetic operations (addition, subtraction, multiplication, division). It also implements
+//! some convenience functions like negation, absolute value and creating from an i64. It also allows
+//! comparisons by deriving the Eq and Ord traits.
+//!
+//! Fixed point arithmetic works similarly to normal integer arithmetic but every number is scaled
+//! by the same number, which we call the scale factor. This library allows to change the scale factor
+//! by simply changing the constant SF. By default it is set to 1 billion, which gives reals with 9
+//! decimal points.
+//! Almost all operations works equally to integer arithmetic, except for multiplication and division.
+//! In multiplication and division the result of the operation needs to be rescaled and rounded. The
+//! range allowed by a real (for the default SF) is [-9223372036.854775808, 9223372036.854775807],
+//! which is simply the range of an i64 but rescaled.
+//!
+//! This library also implements safe math. All reals are an option of an i64, so a real can have the
+//! value 'None'. And all operations check for over/underflow and will return a 'None' as a result when
+//! that happens. A quirk is that, when comparing two reals, 'None' is considered smaller than any
+//! number.
 
 // These are necessary to work with Substrate.
 use parity_codec::{Decode, Encode};
@@ -37,17 +37,12 @@ const MIN: i128 = i64::min_value() as i128;
 pub struct Real(pub Option<i64>);
 
 impl Real {
-    // Transforms an u32 into a real. It scales the input by the scale factor.
-    pub fn from(x: u32) -> Real {
-        Real(x * (SF as u32))
-    }
-
-    // Transforms an i64 into a real. It scales the input by the scale factor.
+    /// Transforms an i64 into a real. It scales the input by the scale factor.
     pub fn from(x: i64) -> Real {
         Real(x.checked_mul(SF as i64))
     }
 
-    // Returns the absolute value of a real. If input is 'None', it returns 'None'.
+    /// Returns the absolute value of a real. If input is 'None', it returns 'None'.
     pub fn abs(self) -> Real {
         if self.0.is_some() {
             Real(self.0.unwrap().checked_abs())
@@ -57,8 +52,8 @@ impl Real {
     }
 }
 
-// Calculates the sum of two reals. If any of the inputs is 'None' (or the result over/underflows),
-// it returns 'None'. It does operator overloading for the symbol '+'.
+/// Calculates the sum of two reals. If any of the inputs is 'None' (or the result over/underflows),
+/// it returns 'None'. It does operator overloading for the symbol '+'.
 impl Add for Real {
     type Output = Real;
 
@@ -71,8 +66,8 @@ impl Add for Real {
     }
 }
 
-// Calculates the division of two reals. If any of the inputs is 'None' (or the result
-// over/underflows), it returns 'None'. It does operator overloading for the symbol '/'.
+/// Calculates the division of two reals. If any of the inputs is 'None' (or the result
+/// over/underflows), it returns 'None'. It does operator overloading for the symbol '/'.
 impl Div for Real {
     type Output = Real;
 
@@ -91,7 +86,7 @@ impl Div for Real {
             let mut c = a * SF;
 
             // Calculating the remainder.
-            let r = (c % b);
+            let r = c % b;
 
             // Dividing by the divisor.
             c /= b;
@@ -115,8 +110,8 @@ impl Div for Real {
     }
 }
 
-// Calculates the multiplication of two reals. If any of the inputs is 'None' (or the result
-// over/underflows), it returns 'None'. It does operator overloading for the symbol '*'.
+/// Calculates the multiplication of two reals. If any of the inputs is 'None' (or the result
+/// over/underflows), it returns 'None'. It does operator overloading for the symbol '*'.
 impl Mul for Real {
     type Output = Real;
 
@@ -130,7 +125,7 @@ impl Mul for Real {
             let mut c = a * b;
 
             // Calculating the remainder.
-            let r = (c % SF);
+            let r = c % SF;
 
             // Dividing by the scale factor.
             c /= SF;
@@ -154,9 +149,9 @@ impl Mul for Real {
     }
 }
 
-// Calculates the negation of a real. If the input is 'None' (or the result
-// overflows which is possible if the input is -2^63/SF), it returns 'None'.
-// It does operator overloading for the symbol '-'.
+/// Calculates the negation of a real. If the input is 'None' (or the result
+/// overflows which is possible if the input is -2^63/SF), it returns 'None'.
+/// It does operator overloading for the symbol '-'.
 impl Neg for Real {
     type Output = Real;
 
@@ -169,8 +164,8 @@ impl Neg for Real {
     }
 }
 
-// Calculates the subtraction of two reals. If any of the inputs is 'None' (or the result
-// over/underflows), it returns 'None'. It does operator overloading for the symbol '-'.
+/// Calculates the subtraction of two reals. If any of the inputs is 'None' (or the result
+/// over/underflows), it returns 'None'. It does operator overloading for the symbol '-'.
 impl Sub for Real {
     type Output = Real;
 
