d1dcb1f325f46d8fb7debd9d0b2a77ab61766c12
retotrinkler <reto@trinkler.software>
2019-06-20 16:20:06 +0200

    Update docs script to be easier consumable by CD service

--- docs.sh
+++ docs.sh
@@ -1,26 +1,14 @@
 #!/usr/bin/env bash
 
 # Define Variables
-LIBS="actus reals time"
-SRC="krml"
-
 PROJECT_ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
-
-export CARGO_INCREMENTAL=0
 
 bold=$(tput bold)
 normal=$(tput sgr0)
 
-# Save current directory.
-pushd . >/dev/null
-
-for LIB in ${LIBS}
-do
-  echo "${bold}Building static doc site for $SRC_$LIB...${normal}"
-  cd "$PROJECT_ROOT/$SRC/$LIB"
-  # Build docs
-  cargo doc --lib
-done
-
-# Restore initial directory.
-popd >/dev/null
+echo "${bold}Building static doc site...${normal}"
+# Build docs
+cargo doc
+# Move to docs folder to deploy
+mkdir docs
+cp -r target/doc/* docs
