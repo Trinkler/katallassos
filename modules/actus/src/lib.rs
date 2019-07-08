698b75fee8ca60ed98adf0da77fd1932ebb94944
brunoffranca <bruno@franca.xyz>
2019-07-08 21:16:11 +0100

    In the actus module: 1- Added a check to see if an id is already taken. 2- Reorganized contract_state.rs, attributes.rs, variables.rs and contract_events.rs into it's own folder. 3- Empty tests are now working.

--- modules/actus/src/lib.rs
+++ modules/actus/src/lib.rs
@@ -20,18 +20,12 @@ use support::{decl_event, decl_module, decl_storage, dispatch::Result, StorageMa
 use time::{Time, UncheckedTime};
 
 // Importing the rest of the files in this crate.
-mod attributes;
-mod contract_events;
 mod contract_state;
 mod contracts;
 mod utilities;
-mod variables;
-use self::attributes::*;
-use self::contract_events::*;
-use self::contract_state::*;
-use self::contracts::*;
-use self::utilities::*;
-use self::variables::*;
+use contract_state::*;
+use contracts::*;
+use utilities::*;
 
 // Defines an alias for the Result type. It has the name MyResult because Substrate already uses
 // the name Result for their own type Result<(), &'static str>.
@@ -71,16 +65,17 @@ decl_module! {
             // Getting the contract ID.
             let id = attributes.contract_id;
 
-            // TODO: Check if id is available.
+            // Checking if id is available.
+            if <ContractStates<T>>::exists(id) {
+                // TODO: Get current time.
+                let t0 = Time::from_values(2019, 07, 04, 00, 00, 00);
 
-            // TODO: Get current time.
-            let t0 = Time::from_values(2019, 07, 04, 00, 00, 00);
+                // Calculating the initial contract state.
+                let state = contracts::initialize(t0, attributes)?;
 
-            // Calculating the initial contract state.
-            let state = contracts::initialize(t0, attributes)?;
-
-            // Storing the contract state.
-            <ContractStates<T>>::insert(id, state);
+                // Storing the contract state.
+                <ContractStates<T>>::insert(id, state);
+            }
 
             Ok(())
         }
@@ -88,58 +83,58 @@ decl_module! {
 }
 
 // tests for this module
-// #[cfg(test)]
-// mod tests {
-//     use super::*;
-//
-//     use primitives::{Blake2Hasher, H256};
-//     use runtime_io::with_externalities;
-//     use runtime_primitives::{
-//         testing::{Digest, DigestItem, Header},
-//         traits::{BlakeTwo256, IdentityLookup},
-//         BuildStorage,
-//     };
-//     use support::{assert_ok, impl_outer_origin};
-//
-//     impl_outer_origin! {
-//         pub enum Origin for Test {}
-//     }
-//
-//     // For testing the module, we construct most of a mock runtime. This means
-//     // first constructing a configuration type (`Test`) which `impl`s each of the
-//     // configuration traits of modules we want to use.
-//     #[derive(Clone, Eq, PartialEq)]
-//     pub struct Test;
-//     impl system::Trait for Test {
-//         type Origin = Origin;
-//         type Index = u64;
-//         type BlockNumber = u64;
-//         type Hash = H256;
-//         type Hashing = BlakeTwo256;
-//         type Digest = Digest;
-//         type AccountId = u64;
-//         type Lookup = IdentityLookup<Self::AccountId>;
-//         type Header = Header;
-//         type Event = ();
-//         type Log = DigestItem;
-//     }
-//     impl Trait for Test {
-//         type Event = ();
-//     }
-//     type ACTUS = Module<Test>;
-//
-//     // This function basically just builds a genesis storage key/value store according to
-//     // our desired mockup.
-//     fn new_test_ext() -> runtime_io::TestExternalities<Blake2Hasher> {
-//         system::GenesisConfig::<Test>::default()
-//             .build_storage()
-//             .unwrap()
-//             .0
-//             .into()
-//     }
-//
-//     #[test]
-//     fn it_works_for_default_value() {
-//         with_externalities(&mut new_test_ext(), || {});
-//     }
-// }
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    use primitives::{Blake2Hasher, H256};
+    use runtime_io::with_externalities;
+    use runtime_primitives::{
+        testing::{Digest, DigestItem, Header},
+        traits::{BlakeTwo256, IdentityLookup},
+        BuildStorage,
+    };
+    use support::{assert_ok, impl_outer_origin};
+
+    impl_outer_origin! {
+        pub enum Origin for Test {}
+    }
+
+    // For testing the module, we construct most of a mock runtime. This means
+    // first constructing a configuration type (`Test`) which `impl`s each of the
+    // configuration traits of modules we want to use.
+    #[derive(Clone, Eq, PartialEq)]
+    pub struct Test;
+    impl system::Trait for Test {
+        type Origin = Origin;
+        type Index = u64;
+        type BlockNumber = u64;
+        type Hash = H256;
+        type Hashing = BlakeTwo256;
+        type Digest = Digest;
+        type AccountId = u64;
+        type Lookup = IdentityLookup<Self::AccountId>;
+        type Header = Header;
+        type Event = ();
+        type Log = DigestItem;
+    }
+    impl Trait for Test {
+        type Event = ();
+    }
+    type ACTUS = Module<Test>;
+
+    // This function basically just builds a genesis storage key/value store according to
+    // our desired mockup.
+    fn new_test_ext() -> runtime_io::TestExternalities<Blake2Hasher> {
+        system::GenesisConfig::<Test>::default()
+            .build_storage()
+            .unwrap()
+            .0
+            .into()
+    }
+
+    #[test]
+    fn it_works_for_default_value() {
+        with_externalities(&mut new_test_ext(), || {});
+    }
+}
