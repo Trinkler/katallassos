0e0c5e515a978c7d10f8299efddaf21fcf8d8d1d
retotrinkler <reto@trinkler.software>
2019-08-22 21:14:52 +0200

    1) 2 second block time for testnet 2) Improves UX testing

--- node/src/chain_spec.rs
+++ node/src/chain_spec.rs
@@ -138,7 +138,7 @@ fn testnet_genesis(
 		}),
 		system: None,
 		timestamp: Some(TimestampConfig {
-			minimum_period: 5, // 10 second block time.
+			minimum_period: 1, // 10 second block time.
 		}),
 		indices: Some(IndicesConfig {
 			ids: endowed_accounts.clone(),
