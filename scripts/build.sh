8a1eebac7767a19d8baa9a81df39d6ac23f4862a
retotrinkler <reto@trinkler.software>
2019-08-30 00:40:13 +0200

    1) Remove tput dependency in build script fixes #62 2) Semver versioning fixes #63

--- scripts/build.sh
+++ scripts/build.sh
@@ -6,15 +6,12 @@ PROJECT_ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )/.." >/dev/null && pwd )"
 
 export CARGO_INCREMENTAL=0
 
-bold=$(tput bold)
-normal=$(tput sgr0)
-
 # Save current directory.
 pushd . >/dev/null
 
 for SRC in node/runtime/wasm
 do
-  echo "${bold}Building webassembly binary in $SRC...${normal}"
+  echo "Building webassembly binary in $SRC..."
   cd "$PROJECT_ROOT/$SRC"
 
   ./build.sh
