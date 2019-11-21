b73f945c837f3a04d0fb1ed77a8921601d8c6583
retotrinkler <reto@trinkler.software>
2019-11-21 19:39:13 +0100

    Reorg scripts

--- node/scripts/init.sh
+++ node/scripts/init.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+/env bash
 
 set -e
 
@@ -10,7 +10,3 @@ if [ -z $CI_PROJECT_NAME ] ; then
 fi
 
 rustup target add wasm32-unknown-unknown --toolchain nightly
-
-# Install wasm-gc. It's useful for stripping slimming down wasm binaries.
-command -v wasm-gc || \
-	cargo +nightly install --git https://github.com/alexcrichton/wasm-gc --force
