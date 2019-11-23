0b4810bf1857331601e5be120af71c11f42b809c
retotrinkler <reto@trinkler.software>
2019-11-23 19:02:50 +0100

    1) Fix substrate commit 2) Update compile script

--- node/scripts/compile.sh
+++ node/scripts/compile.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-./scripts/init.sh # Build wasm binaries
+./node/scripts/init.sh # Build wasm binaries
 cargo build # Build native binaries in debug mode
 ./target/debug/katalchain purge-chain -y --dev # Remove the whole chain data
 ./target/debug/katalchain --dev # Run chain in dev mode
