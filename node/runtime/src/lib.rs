2f8ea99fd517c00750b20ab067f635004e7c74dd
retotrinkler <reto@trinkler.software>
2019-11-25 19:19:42 +0100

    Use [authoring version 4](https://github.com/paritytech/substrate/blob/c0f9a21a6d54815de0b1fe7d482a39981b4a238b/primitives/sr-primitives/src/generic/unchecked_extrinsic.rs#L27). This should fix the issue with transaction signing.

--- node/runtime/src/lib.rs
+++ node/runtime/src/lib.rs
@@ -102,7 +102,11 @@ pub mod opaque {
 pub const VERSION: RuntimeVersion = RuntimeVersion {
     spec_name: create_runtime_str!("katalchain"),
     impl_name: create_runtime_str!("katalchain"),
-    authoring_version: 1,
+    authoring_version: 4,
+    // Per convention: if the runtime behavior changes, increment spec_version
+    // and set impl_version to equal spec_version. If only runtime
+    // implementation changes and behavior does not, then leave spec_version as
+    // is and increment impl_version.
     spec_version: 1,
     impl_version: 1,
     apis: RUNTIME_API_VERSIONS,
